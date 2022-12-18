dd3a40b4a8af98d08b754170d516b8b0654651f4
Rojahno <andreas_sunde97@hotmail.com>
2022-12-19 00:24:08 +0100

    Added @media for scaling

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,7 +6,7 @@ import bachelor from "../../assets/bachelor.png";
 import github from "../../assets/github.svg";
 
 const rojoText =
-    "This repository is a hobby project where I have created some commonly used " +
+    "This is a hobby project where I have created some commonly used " +
     "react components. The goal of the project was to learn more about React and to " +
     "create reusable components that I could use for the future.";
 const aStarText =
@@ -15,7 +15,7 @@ const aStarText =
     "to artifical intelligence course TDT4136.";
 
 const bachelorText =
-    "This Project was created as a Bachelor Thesis at Norwegian University of Science " +
+    "This project was created as a Bachelor Thesis at Norwegian University of Science " +
     "and Technology in Ålesund. The aim of the project was to give patients with ALS a way " +
     "to perform simple tasks and communicate with the people around them using the Emotiv Epoc X Headset";
 
@@ -24,38 +24,50 @@ export const Projects = () => {
         <div className="project-container">
             <div className="project">
                 <img className="project-image" src={rojo} alt="rojo component library" />
-                <div>
+                <div className="project-content-container">
                     <h2 className="project-title">Rojo component library</h2>
                     <p className="project-text">{rojoText}</p>
+                    <a
+                        href="https://github.com/rojahno/Rojos"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="gihub-button">
+                        <img className="github-icon" src={github} alt="github icon" />
+                        View project
+                    </a>
                 </div>
-                <a href="https://github.com/rojahno/Rojos" target="_blank" className="gihub-button">
-                    <img className="github-icon" src={github} alt="github icon" />
-                    View project
-                </a>
             </div>
 
             <div className="project">
                 <img className="project-image" src={aStar} alt="a star algorithm" />
-                <div>
-                    <h2 className="project-title">A-star</h2>
+                <div className="project-content-container">
+                    <h2 className="project-title">A-star algorithm</h2>
                     <p className="project-text">{aStarText}</p>
+                    <a
+                        href="https://github.com/rojahno/a-star-pathfinding"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="gihub-button">
+                        <img className="github-icon" src={github} alt="github icon" />
+                        {"View project"}
+                    </a>
                 </div>
-                <a href="https://github.com/rojahno/a-star-pathfinding" target="_blank" className="gihub-button">
-                    <img className="github-icon" src={github} alt="github icon" />
-                    View project
-                </a>
             </div>
 
             <div className="project">
                 <img className="project-image" src={bachelor} alt="bachelor raport cover" />
-                <div>
+                <div className="project-content-container">
                     <h2 className="project-title">EEG for ALS Bachelor Thesis</h2>
                     <p className="project-text">{bachelorText}</p>
+                    <a
+                        href="https://github.com/rojahno/Enabled-desktop"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="gihub-button">
+                        <img className="github-icon" src={github} alt="github icon" />
+                        {"View project"}
+                    </a>
                 </div>
-                <a href="https://github.com/rojahno/Enabled-desktop" target="_blank" className="gihub-button">
-                    <img className="github-icon" src={github} alt="github icon" />
-                    View project
-                </a>
             </div>
         </div>
     );
