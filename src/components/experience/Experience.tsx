abfde3feaa9e84a6498fdc251f382fd417709f8f
Andreas <andreass@stacc.com>
2024-03-26 08:57:13 +0100

    change style and cleaned up

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -2,91 +2,29 @@ import { useInView } from "framer-motion";
 import { useRef } from "react";
 import { Accordion } from "../accordion/Accordion";
 import "./Experience.css";
+import { experiences } from "./data";
 
 export const Experience = () => {
     const ref = useRef<HTMLDivElement>(null);
     const isInView = useInView(ref, { once: true });
-
+    
     return (
         <div className="experiences" ref={ref}>
             <div className="entries">
-                <div className="entry">
+                {experiences.map((experience, index) => (
+                    <div className="entry" key={index}>
                     <Accordion
-                        year="2023"
-                        title="Masters degree in computer science"
+                        key={index}
+                        title={experience.title}
+                        year={experience.year}
                         isInView={isInView}
+                        animatoionPixels={-20}
                         animationDelay={1.4}
-                        animatoionPixels={-20}>
-                        <p>
-                            Finished a master's degree from NTNU in Trondheim, Norway. The master's degree is in
-                            computer science with a specialization in artificial intelligence.
-                        </p>
+                    >
+                        <p>{experience.description}</p>
                     </Accordion>
-                </div>
-
-                <div className="entry">
-                    <Accordion
-                        year="2022"
-                        title="Summer intern for Bredvid"
-                        isInView={isInView}
-                        animationDelay={1.6}
-                        animatoionPixels={20}>
-                        <p>
-                        Worked as an IT consultant for Bredvid. My tasks involved creating a web portal with stacks such as React, Kotlin, and Azure. 
-                        </p>
-                    </Accordion>
-                </div>
-                <div className="entry">
-                    <Accordion
-                        year="2021"
-                        title="Bachelors degree in computer science"
-                        isInView={isInView}
-                        animationDelay={1.8}
-                        animatoionPixels={-20}>
-                        <p>
-                            Finished a bachelor's degree from NTNU in Ålesund, Norway.
-                        </p>
-                    </Accordion>
-                </div>
-                <div className="entry">
-                    <Accordion
-                        year="2019-2021"
-                        title="Programmer at Multilux"
-                        isInView={isInView}
-                        animationDelay={2}
-                        animatoionPixels={20}>
-                        <p>
-                        I worked as a programmer for Multilux on their CitySight-project. I was involved in developing a smart-city 
-                        solution where the main goal of the project was to make it easier for municipalities to control street lights and 
-                        save energy through the use of sensors. 
-                        </p>
-                    </Accordion>
-                </div>
-                <div className="entry">
-                    <Accordion
-                        year="2017"
-                        title="Military policy at the Norwegian army"
-                        isInView={isInView}
-                        animationDelay={2.4}
-                        animatoionPixels={20}>
-                        <p>
-                        Worked as a military police soldier at Haakonsvern, Bergen.
-                        </p>
-                    </Accordion>
-                </div>
-                <div className="entry">
-                    <Accordion
-                        year="2012-2016"
-                        title="Retail worker at Spar Kaldnes"
-                        isInView={isInView}
-                        animationDelay={2.6}
-                        animatoionPixels={-20}>
-                        <p>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium sint ipsa provident
-                            cupiditate, molestiae debitis consequuntur aspernatur voluptatum aperiam perferendis.
-                        </p>
-                    </Accordion>
-                </div>
+                    </div>
+                ))}
             </div>
         </div>
     );
---
+++ src/components/experience/data.ts
@@ -0,0 +1,37 @@
+export const experiences = [
+    {
+        year: "2023",
+        title: "System developer Stacc",
+        description: "Worked as a system developer for Stacc. My tasks involved developing a web portal for a customer in the finance sector.",
+    },
+    {
+        year: "2023",
+        title: "Masters degree in computer science",
+        description: "Finished a master's degree from NTNU in Trondheim, Norway. The master's degree is in computer science with a specialization in artificial intelligence.",
+    },
+    {
+        year: "2022",
+        title: "Summer intern for Bredvid",
+        description: "Worked as an IT consultant for Bredvid. My tasks involved creating a web portal with stacks such as React, Kotlin, and Azure.",
+    },
+    {
+        year: "2021",
+        title: "Bachelors degree in computer science",
+        description: "Finished a bachelor's degree from NTNU in Ålesund, Norway.",
+    },
+    {
+        year: "2019-2021",
+        title: "Programmer at Multilux",
+        description: "I worked as a programmer for Multilux on their CitySight-project. I was involved in developing a smart-city solution where the main goal of the project was to make it easier for municipalities to control street lights and save energy through the use of sensors.",
+    },
+    {
+        year: "2017",
+        title: "Military policy at the Norwegian army",
+        description: "Worked as a military police soldier at Haakonsvern, Bergen.",
+    },
+    {
+        year: "2012-2016",
+        title: "Retail worker at Spar Kaldnes",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium sint ipsa provident cupiditate, molestiae debitis consequuntur aspernatur voluptatum aperiam perferendis.",
+    },
+];
