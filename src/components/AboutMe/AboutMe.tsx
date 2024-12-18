c7096c6760e852d6d985cd0a966a1e9996476c9c
Andreas <andreass@stacc.com>
2024-12-18 18:00:08 +0100

    small change

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -2,34 +2,34 @@ import { useInView } from "framer-motion";
 import { useRef } from "react";
 import "./AboutMe.css";
 
-const ageText = "My name is Andreas Sunde and i'm 26 years old.";
+const ageText = "My name is Andreas Sunde";
 const educationText =
-    "I have a bachelor degree in computer science from NTNU, where I " +
-    "wrote about EEG equpiment and how to use it to help ALS patients. I am " +
-    "currently studying a master degree in computer science at NTNU, where I specialize in artificial intelligence.";
+  "I have a bachelor degree in computer science from NTNU, where I " +
+  "wrote about EEG equpiment and how to use it to help ALS patients. I am " +
+  "currently studying a master degree in computer science at NTNU, where I specialize in artificial intelligence.";
 const workText =
-    "I worked as a software developer for Multilux AS for 2 years " +
-    "and I was a part of a team that developed a smart-city solution called CitySight. " +
-    "I also worked as an IT-consulent during the summer of 2022, and I have been a student assistant in three different courses at NTNU.";
+  "I worked as a software developer for Multilux AS for 2 years " +
+  "and I was a part of a team that developed a smart-city solution called CitySight. " +
+  "I also worked as an IT-consulent during the summer of 2022, and I have been a student assistant in three different courses at NTNU.";
 
-const hobbyText = "Apart from work and school, I like to spend time with my friends and work out.  ";
+const hobbyText = "I am passionate about technology and love to learn new things.";
 
 export const AboutMe = () => {
-    const ref = useRef<HTMLDivElement>(null);
-    const isInView = useInView(ref, { once: true });
-    return (
-        <div
-            ref={ref}
-            className="about-me"
-            style={{
-                transform: isInView ? "none" : "translateX(-20px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-            }}>
-            <p className="about-text">{ageText}</p>
-            <p className="about-text">{educationText}</p>
-            <p className="about-text">{workText}</p>
-            <p className="about-text">{hobbyText}</p>
-        </div>
-    );
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once: true });
+  return (
+    <div
+      ref={ref}
+      className="about-me"
+      style={{
+        transform: isInView ? "none" : "translateX(-20px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+      }}>
+      <p className="about-text">{ageText}</p>
+      <p className="about-text">{educationText}</p>
+      <p className="about-text">{workText}</p>
+      <p className="about-text">{hobbyText}</p>
+    </div>
+  );
 };
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,78 +1,78 @@
 import "./Profile.css";
 import linkedIn from "../../assets/linkedin.svg";
 import gituhb from "../../assets/github.svg";
-import profilePicture from "../../assets/profile_picture.jpeg";
+import profilePicture from "../../assets/profile_picture.jpg";
 import { useEffect, useRef, useState } from "react";
 
-const profileText = "Computer science student at NTNU in Trondheim, Norway.";
+const profileText = "System developer at Stacc and passionate about technology"
 
 export const Profile = () => {
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const name = "ANDREAS";
-    const nameRef = useRef<HTMLHeadingElement>(null);
-    const [nameText, setNameText] = useState(name);
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const name = "ANDREAS";
+  const nameRef = useRef<HTMLHeadingElement>(null);
+  const [nameText, setNameText] = useState(name);
 
-    useEffect(() => {
-        let iteration = 0;
-        function scrambledName() {
-            let newName = nameText
-                .split("")
-                .map((_letter, index) => {
-                    if (index < iteration) {
-                        return name[index];
-                    }
-                    return letters[Math.floor(Math.random() * letters.length)];
-                })
-                .join("");
-            iteration += 1 / 4;
-            return newName;
+  useEffect(() => {
+    let iteration = 0;
+    function scrambledName() {
+      const newName = nameText
+        .split("")
+        .map((_letter, index) => {
+          if (index < iteration) {
+            return name[index];
+          }
+          return letters[Math.floor(Math.random() * letters.length)];
+        })
+        .join("");
+      iteration += 1 / 4;
+      return newName;
+    }
+
+    function beginScrambling() {
+      const interval = setInterval(() => {
+        if (nameRef.current) {
+          const newName = scrambledName();
+          if (iteration === name.length) {
+            clearInterval(interval);
+          }
+          setNameText(newName);
         }
+      }, 30);
+      return () => clearInterval(interval);
+    }
 
-        function beginScrambling() {
-            let interval = setInterval(() => {
-                if (nameRef.current) {
-                    let newName = scrambledName();
-                    if (iteration === name.length) {
-                        clearInterval(interval);
-                    }
-                    setNameText(newName);
-                }
-            }, 30);
-            return () => clearInterval(interval);
-        }
+    const beginInterval = setInterval(() => {
+      beginScrambling();
+      clearInterval(beginInterval);
+    }, 2000);
+    return () => clearInterval(beginInterval);
+    // eslint-disable-next-line
+  }, []);
 
-        let beginInterval = setInterval(() => {
-            beginScrambling();
-            clearInterval(beginInterval);
-        }, 2000);
-        return () => clearInterval(beginInterval);
-        // eslint-disable-next-line
-    }, []);
-
-    return (
-        <div id="profile">
-            <div id="profile-container">
-                <div id="img-container">
-                    <img id="profile-image" src={profilePicture} alt="" />
-                    <h2 id="profile-name" ref={nameRef}>
-                        HELLO,
-                        <br />
-                        IM {nameText}
-                    </h2>
-                </div>
-                <p id="text-container">{profileText}</p>
-                <div id="button-container">
-                    <a
-                        href="https://www.linkedin.com/in/andreas-rojahn-sunde-64024521a/"
-                        target="_blank"
-                        rel="noreferrer">
-                        <img src={linkedIn} id="profile-button" alt="linkedin" />
-                    </a>
-                    <a href="https://github.com/rojahno" target="_blank" rel="noreferrer">
-                        <img src={gituhb} id="profile-button" alt="github" />
-                    </a>
-                </div>
-            </div>
+  return (
+    <div id="profile">
+      <div id="profile-container">
+        <div id="img-container">
+          <img id="profile-image" src={profilePicture} alt="" />
+          <h2 id="profile-name" ref={nameRef}>
+            HELLO,
+            <br />
+            IM {nameText}
+          </h2>
         </div>
-    );
+        <p id="text-container">{profileText}</p>
+        <div id="button-container">
+          <a
+            href="https://www.linkedin.com/in/andreas-rojahn-sunde-64024521a/"
+            target="_blank"
+            rel="noreferrer">
+            <img src={linkedIn} id="profile-button" alt="linkedin" />
+          </a>
+          <a href="https://github.com/rojahno" target="_blank" rel="noreferrer">
+            <img src={gituhb} id="profile-button" alt="github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
